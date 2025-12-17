f014596d74729a134c772a49a4afec011bdc0f6f
D. Bohdan <dbohdan@dbohdan.com>
2025-12-17 12:15:06 +0000

    feat(compl): update options

--- completions/age-edit.fish
+++ completions/age-edit.fish
@@ -1,7 +1,12 @@
 complete -c age-edit -s a -l armor -d 'Write armored age file'
-complete -c age-edit -s e -l editor -d 'Command to use for editing the encrypted file' -r
+complete -c age-edit -s c -l command -d 'Editor command' -r
+complete -c age-edit -l decode -d 'Filter command after decryption' -r
+complete -c age-edit -s e -l editor -d 'Editor executable' -r
+complete -c age-edit -l encode -d 'Filter command before encryption' -r
+complete -c age-edit -s f -l force -d 'Force re-encryption'
+complete -c age-edit -s L -l no-lock -d 'Do not lock encrypted file'
 complete -c age-edit -s M -l no-memlock -d 'Disable mlockall(2) that prevents swapping'
-complete -c age-edit -s r -l read-only -d 'Discard all changes'
+complete -c age-edit -s r -l read-only -d 'Make the temporary file read-only and discard all changes'
 complete -c age-edit -s t -l temp-dir -d 'Temporary directory prefix' -r
 complete -c age-edit -s V -l version -d 'Report the program version and exit'
 complete -c age-edit -s w -l warn -d 'Warn if editor exits after less than N seconds' -r
